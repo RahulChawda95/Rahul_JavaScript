7053e1849a1903e1828a501422034a65709ab938
RahulChawda95 <115606738+RahulChawda95@users.noreply.github.com>
2022-12-21 13:04:41 +0530

    Add

---
+++ Lab_Work/04-Data_type-External-JS/js/data-type.js
@@ -0,0 +1,22 @@
+document.write("<h1>This Is External JS......!</h1>")
+document.write("<h2>Type of Variable Show Inside Console</h2>")
+var x=-78
+
+console.log(x);
+console.log(typeof(x));
+
+var switch1= '-78  %$^&875';
+
+console.log(switch1);
+console.log(typeof(switch1));
+
+
+
+var _chk= true;
+
+console.log(_chk);
+console.log(typeof(_chk));
+
+var rahul;
+console.log(rahul)
+console.log(typeof(rahul))
